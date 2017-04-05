b9ec2bbf16c38a4f3cc63a7d24fcea9bcdd63d59
Jordan Scholten <schojo13@gmail.com>
2017-04-05 14:14:29 -0700

    Display empty board

---
+++ Tic-Tac-Toe/game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int number;
+	string board = "\n _ _ _ \n _ _ _ \n _ _ _ \n";
+
+	cout << board << "\n Number please: ";
+	cin >> number;
+
+	return 0;
+}
